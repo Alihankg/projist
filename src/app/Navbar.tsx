da866aef010cddf45950da5278b3ed11ec935b78
alikhan <kebrun2017@gmail.com>
2024-04-30 09:10:50 +0300

    initial

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+	return (
+		<div className=" h-1/5 bg-slate-200 p-4 mx-auto text-base-300">
+			<div className="container">Projist Spor Rehber</div>
+		</div>
+	)
+}
---
+++ src/app/ai.ts
@@ -0,0 +1,62 @@
+import dotenv from 'dotenv'
+dotenv.config()
+const {
+	GoogleGenerativeAI,
+	HarmCategory,
+	HarmBlockThreshold,
+} = require('@google/generative-ai')
+const genAI = new GoogleGenerativeAI('AIzaSyCwYFa04IhaPz4KG0w9NM0R8ScG3ksQ8JQ')
+const model = genAI.getGenerativeModel({ model: 'gemini-1.0-pro' })
+
+const generationConfig = {
+	temperature: 1,
+	topK: 0,
+	topP: 0.95,
+	maxOutputTokens: 8192,
+}
+
+const safetySettings = [
+	{
+		category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+		threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+	},
+	{
+		category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+		threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+	},
+	{
+		category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+		threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+	},
+	{
+		category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+		threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+	},
+]
+const history = [
+	{
+		role: 'user',
+		parts: [
+			{
+				text: 'Deneyimli bir spor koçusun ve insanlara spor hakkında rehberlik edeceksiniz. Cevapları olabildiğince açıklayıcı ve basit ver. Anladıysanız tamam deyin.',
+			},
+		],
+	},
+	{
+		role: 'model',
+		parts: [{ text: 'Tamam.' }],
+	},
+]
+const chat = model.startChat({
+	generationConfig,
+	safetySettings,
+	history: history,
+})
+
+async function send(msg: string) {
+	const result = await chat.sendMessage(msg)
+	const response = result.response
+	return response.text()
+}
+
+export { send }
